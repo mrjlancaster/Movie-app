bf3d84393efda6614357d179bc3b85f68b51217e
mrjlancaster <lancast001@gmail.com>
2020-12-20 22:33:12 -0600

    create functions to handle data

--- main.js
+++ main.js
@@ -1,35 +1,48 @@
 const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search');
+
 const search_url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY;
 
 const imageBaseURL = 'https://image.tmdb.org/t/p/w500/';
 
-// Handle movie search
-searchButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const value = searchInput.value;
-    const newURL = search_url + '&query=' + value;
+// Get data
+const getData = (url, func) => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => func(data))
+}
+
+// Display data
+const displayMovies = (obj) => {
     const output = document.querySelector('.output');
-
+    const data = [obj];
+    const movies = data[0].results;
+    
     let movie = '';
+    movies.forEach(item => {
+        movie +=`
+            <div class="template__wrapper">
+                <img src=${imageBaseURL + item.poster_path} class="movie__poster" alt="" />
+                <h3 class="movie__title">${item.title}</h3>
+                <button class="more__details">More details</button>
+            </div>`;
+    })
 
-    fetch(newURL)
-        .then(results => results.json())
-        .then(data => {
-            const results = [data];
-            const movies = results[0].results;
+    // Output results
+    output.innerHTML = movie;
 
-            movies.forEach(item => {
-                movie += `
-                <div class="template__wrapper">
-                    <img src=${imageBaseURL + item.poster_path} class="movie__poster" alt="" />
-                    <h3 class="movie__title">${item.title}</h3>
-                    <button class="more__details">More details</button>
-                </div>`;
-                output.innerHTML = movie;
-            })
-        });
+    // Clear input field
+    searchInput.value = '';
+}
 
-        // clear input field
-        searchInput.value = '';
-})
+// Handle movie search
+const searchMovie = () => {
+    searchButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const value = searchInput.value;
+        const newURL = search_url + '&query=' + value;
+        getData(newURL, displayMovies);
+    })
+}
+
+searchMovie();
