cfdf35d91df49329e1994ddfcfb2a7c9005c7dbc
mrjlancaster <lancast001@gmail.com>
2020-12-21 01:02:46 -0600

    fetch data for upcoming movies

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search');
 
 const search_url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY;
-
+const upcoming_URL = 'https://api.themoviedb.org/3/movie/upcoming?api_key=' + API_KEY;
 const imageBaseURL = 'https://image.tmdb.org/t/p/w500/';
+
+
+// &language=en-US&page=1
 
 // Get data
 const getData = (url, func) => {
@@ -12,8 +15,8 @@ const getData = (url, func) => {
         .then(data => func(data))
 }
 
-// Display data
-const displayMovies = (obj) => {
+// Display search
+const displaySearch = (obj) => {
     const output = document.querySelector('.output');
     const data = [obj];
     const movies = data[0].results;
@@ -23,7 +26,7 @@ const displayMovies = (obj) => {
         movie +=`
             <div class="template__wrapper">
                 <img src=${imageBaseURL + item.poster_path} class="movie__poster" alt="" />
-                <h3 class="movie__title">${item.title}</h3>
+                <h4 class="movie__title">${item.title}</h4>
                 <button class="more__details">More details</button>
             </div>`;
     })
@@ -35,14 +38,38 @@ const displayMovies = (obj) => {
     searchInput.value = '';
 }
 
+// Display upcoming movies
+const displayUpcoming = (obj) => {
+    const upcoming = document.querySelector('.upcoming');
+    const movies = obj.results;
+    
+    let movie = '';
+    movies.forEach(item => {
+        movie +=`
+            <div class="upcoming__card--wrapper">
+                <img src=${imageBaseURL + item.poster_path} class="movie__poster" alt="" />
+            </div>`;
+    })
+
+    // Output results
+    upcoming.innerHTML = movie;
+}
+
 // Handle movie search
 const searchMovie = () => {
     searchButton.addEventListener('click', (e) => {
         e.preventDefault();
         const value = searchInput.value;
         const newURL = search_url + '&query=' + value;
-        getData(newURL, displayMovies);
+        getData(newURL, displaySearch);
     })
 }
 
+// Handle upcoming movies
+const handleUpcomingMovies = () => {
+    getData(upcoming_URL, displayUpcoming);
+}
+
+
+handleUpcomingMovies();
 searchMovie();
