06d8c018b1e97d25d1393d83a083d282726de43d
mrjlancaster <lancast001@gmail.com>
2020-12-20 19:39:15 -0600

    create search function and setup output

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search');
-const api_url = 'https://api.themoviedb.org/3/search/movie?api_key=' + movie_API;
+const search_url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY;
 
+const imageBaseURL = 'https://image.tmdb.org/t/p/w500/';
 
+// Handle movie search
 searchButton.addEventListener('click', (e) => {
     e.preventDefault();
     const value = searchInput.value;
-    const newURL = api_url + '&query=' + value;
+    const newURL = search_url + '&query=' + value;
+    const output = document.querySelector('.output');
+
+    let movie = '';
 
     fetch(newURL)
         .then(results => results.json())
-        .then(data => console.log(data))
+        .then(data => {
+            const results = [data];
+            const movies = results[0].results;
+
+            movies.forEach(item => {
+                movie += `
+                <div class="template__wrapper">
+                    <img src=${imageBaseURL + item.poster_path} class="movie__poster" alt="" />
+                    <h3 class="movie__title">${item.title}</h3>
+                    <button class="more__details">More details</button>
+                </div>`;
+                output.innerHTML = movie;
+            })
+        });
+
+        // clear input field
+        searchInput.value = '';
 })
