d0fbc3c5415261398acbc7934dc3db4b958573f4
Jonathan Lancaster <lancast001@gmail.com>
2023-02-16 16:31:06 -0600

    create search component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,6 @@
-import { useState } from "react";
-import { GoSearch } from "react-icons/go";
-import { apiInstance } from "./api/axios";
+import Search from "./Search";
 
 const Header = () => {
-  const [searchInput, setSearchInput] = useState("");
-
-  const handleSearch = async () => {
-    try {
-      const splitSearch = searchInput.split(" ");
-      let searchValue;
-
-      if (splitSearch.length > 1) {
-        for (let item of splitSearch) {
-          searchValue[0] += item;
-        }
-      }
-
-      console.log(searchValue);
-
-      const res = await apiInstance.get(`/search/movie?&query=${searchInput}`);
-      //   console.log(res);
-      setSearchInput("");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <header>
       <span>
@@ -34,21 +9,7 @@ const Header = () => {
           <span className="logo__app">App</span>
         </a>
       </span>
-      <div className="search__input--container">
-        <div className="search__bar">
-          <input
-            type="text"
-            className="search__input"
-            value={searchInput}
-            placeholder="Search..."
-            onChange={(e) => setSearchInput(e.target.value)}
-          />
-          <button onClick={handleSearch} type="button" className="search">
-            {/* <i className="fas fa-search"></i> */}
-            <GoSearch />
-          </button>
-        </div>
-      </div>
+      <Search />
     </header>
   );
 };
